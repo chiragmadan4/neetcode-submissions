947ee6da6c8eddd3cbb37c9ad3684e82f0391c73
Chirag Madan <chiragmadan4@gmail.com>
2026-06-02 08:59:08 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.size() == 0) {
+                    return false;
+                }
+                char top = stack.peek();
+                if (getOpp(top) == c) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.size() == 0;
+        
+    }
+    public char getOpp(char c) {
+        if (c == '(') {
+            return ')';
+        }
+        if (c == '{') {
+            return '}';
+        }
+        if (c == '[') {
+            return ']';
+        }
+        return 'a';
+    }
+}
