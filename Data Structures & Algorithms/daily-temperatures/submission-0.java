ead42805d751fdc95aa5a3e0163b8da8b722ad66
Chirag Madan <chiragmadan4@gmail.com>
2026-06-02 09:18:36 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,36 @@
+class Pair {
+    int val;
+    int idx;
+
+    public Pair(int val, int idx) {
+        this.val = val;
+        this.idx = idx;
+    }
+}
+
+class Solution {
+    public int[] dailyTemperatures(int[] arr) {
+        Stack<Pair> stack = new Stack<>();
+        int ans[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) {
+                stack.push(new Pair(arr[i], i));
+                continue;
+            }
+            Pair top = stack.peek();
+            if (arr[i] <= top.val) {
+                stack.push(new Pair(arr[i], i));
+                continue;
+            }
+            while (!stack.isEmpty() && arr[i] > top.val) {
+                stack.pop();
+                ans[top.idx] = i - top.idx;
+                if (stack.size() > 0) {
+                    top = stack.peek();
+                }
+            }
+            stack.push(new Pair(arr[i], i));
+        }
+        return ans;
+    }
+}
