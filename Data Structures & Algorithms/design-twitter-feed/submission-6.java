3af162305970c942b7b25070a5ca7514548a630e
Chirag Madan <chiragmadan4@gmail.com>
2026-06-07 18:18:13 +0530

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.java
@@ -0,0 +1,65 @@
+class Post {
+    int i;
+    int t;
+    Post(int i, int t) {
+        this.i = i;
+        this.t = t;
+    }
+}
+class Twitter {
+
+    Map<Integer, List<Post>> userPosts;
+    Map<Integer, Set<Integer>> userFollowings;
+    int t = 1;
+    public Twitter() {
+        userPosts = new HashMap<>();
+        userFollowings = new HashMap<>();
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        userPosts.putIfAbsent(userId, new ArrayList<>());
+        Post p = new Post(tweetId, t);
+        t++;
+        userPosts.get(userId).add(p);
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        Set<Integer> followings = userFollowings.getOrDefault(userId, new HashSet<>());
+        List<Post> posts = new ArrayList<>();
+        for (int f: followings) {
+            List<Post> sub = userPosts.getOrDefault(f, new ArrayList<>());
+            int size = sub.size();
+            int start = Math.max(0, size - 10);
+            posts.addAll(sub.subList(start, size));
+        }
+        posts.addAll(userPosts.getOrDefault(userId, new ArrayList<>()));
+        PriorityQueue<Post> pq = new PriorityQueue<>(
+            Comparator.comparingInt(p -> p.t)
+        );
+        for (Post p: posts) {
+            pq.add(p);
+            if (pq.size() > 10) {
+                pq.poll();
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(pq.poll().i);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if (followerId == followeeId) {
+            return;
+        }
+        userFollowings.putIfAbsent(followerId, new HashSet<>());
+        userFollowings.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        userFollowings.putIfAbsent(followerId, new HashSet<>());
+        userFollowings.get(followerId).remove(Integer.valueOf(followeeId));
+    }
+}
