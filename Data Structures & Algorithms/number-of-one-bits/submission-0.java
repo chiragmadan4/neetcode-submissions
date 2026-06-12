7ae1e84e1a75f762804e24599ac62bb41c4bc886
Chirag Madan <chiragmadan4@gmail.com>
2026-06-12 09:02:03 +0530

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int ans = 0;
+        for (int i = 0; i <= 31; i++) {
+            int mask = 1 << i;
+            if ((n & mask) > 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
